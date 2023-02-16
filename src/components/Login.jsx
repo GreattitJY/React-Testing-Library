a87d4ead6618ad39cb1b86d1ee4d52163befe390
GreattitJY <great.tit.jaejae@gmail.com>
2023-02-16 10:25:00 +0900

    add: user-event(click, tab, space, enter)

---
+++ src/components/Login.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+
+export default function Login() {
+  const [isLogin, setIsLogin] = useState(false);
+
+  function onClickHandler() {
+    setIsLogin(!isLogin);
+  }
+
+  return <button onClick={onClickHandler}>{isLogin ? "Logout" : "Login"}</button>;
+}
